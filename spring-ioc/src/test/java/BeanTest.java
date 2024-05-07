d48b73dd74ae408a89f69d1b99c512c09f97758c
liubaojun <lbj0609@163.com>
2024-05-07 21:44:58 +0800

    【新增】Spring 加载非自定义MyBatis Bean对象

--- spring-ioc/src/test/java/BeanTest.java
+++ spring-ioc/src/test/java/BeanTest.java
@@ -1,7 +1,16 @@
 import com.alibaba.druid.pool.DruidDataSource;
+import com.mysql.cj.xdevapi.SessionFactory;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class BeanTest {
     /**
@@ -28,4 +37,20 @@ public class BeanTest {
         System.out.println(clazz);
 
     }
+
+    @Test
+    public void test03() throws IOException {
+        InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+        SqlSessionFactory sessionFactory = builder.build(inputStream);
+        SqlSession sqlSession = sessionFactory.openSession();
+        System.out.println(sqlSession);
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        SqlSessionFactory sqlSessionFactory = (SqlSessionFactory) context.getBean("sqlSessionFactory");
+        System.out.println(sqlSessionFactory);
+        SqlSession sqlSession1 = sqlSessionFactory.openSession();
+        System.out.println(sqlSession1);
+    }
+
 }
